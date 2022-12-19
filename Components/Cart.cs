0bcc366ab8210a34becc2484294ee5a99e24296c
Emilija Vaisvalavičiūtė <emilija.vaisvalaviciute@ktu.edu>
2022-12-19 23:36:28 +0200

    Add cart functionality

--- Components/Cart.cs
+++ Components/Cart.cs
@@ -1,21 +1,73 @@
-﻿namespace StatybaServer.Components
+﻿using StatybaServer.Models;
+
+namespace StatybaServer.Components
 {
     public class Cart : ICart
     {
-        public int CartCount { get; set; }
+        private List<Preke> prekes = new List<Preke>();
+        private List<Preke> prekesDistinct = new List<Preke>();
+        public Cart() { }
+
+        public void Add(Preke preke)
+        {
+            var obj = prekes.FirstOrDefault(x => x.IdPreke == preke.IdPreke);
+
+            prekes.Add(preke);
+
+            prekesDistinct = prekes.GroupBy(x => x.IdPreke).Select(x => x.First()).ToList();
+        }
 
         public int GetCount()
         {
-            return CartCount;
+            return prekes.Count;
         }
-        public void Add()
+
+        public int GetCountDistinct()
         {
-            CartCount++;
+            return prekesDistinct.Count;
         }
-        public Cart()
+
+        public int GetPrekeCount(int id)
         {
-            CartCount = 0;
-            //CartCount++;
+            return prekes.Where(x => x.IdPreke == id).Count();
+        }
+
+        public List<Preke> GetPrekes()
+        {
+            return prekes;
+        }
+
+        public List<Preke> GetDistinctPrekes()
+        {
+            return prekesDistinct;
+        }
+
+        public void AddById(int id)
+        {
+            var obj = prekes.FirstOrDefault(x => x.IdPreke == id);
+
+            if(obj != null)
+            {
+                prekes.Add(obj);
+            }
+        }
+
+        public void RemoveById(int id)
+        {
+            var obj = prekes.FirstOrDefault(x => x.IdPreke == id);
+
+            int count = GetPrekeCount(id);
+
+            if (obj != null)
+            {
+                prekes.Remove(obj);
+
+                if(count == 1)
+                {
+                    obj = prekesDistinct.FirstOrDefault(x => x.IdPreke == id);
+                    prekesDistinct.Remove(obj);
+                }
+            }
         }
     }
 }
--- Components/ICart.cs
+++ Components/ICart.cs
@@ -1,8 +1,17 @@
-﻿namespace StatybaServer.Components
+﻿using StatybaServer.Models;
+
+namespace StatybaServer.Components
 {
     public interface ICart
     {
         int GetCount();
-        void Add();
+        int GetCountDistinct();
+        int GetPrekeCount(int id);
+        List<Preke> GetPrekes();
+        List<Preke> GetDistinctPrekes();
+        void Add(Preke preke);
+        void AddById(int id);
+        void RemoveById(int id);
+
     }
 }
